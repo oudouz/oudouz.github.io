a8142aaf156d6b29ffb1a7f1ff63c3e13c4760e3
Aymen Chaouki <oudouz@gmail.com>
2017-05-19 12:06:42 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
+var config = {  
+	apiKey: "AIzaSyCeLb593nc4IMYNMNyr34GXwAKPK4nR_-Y",
+	authDomain: "web-app-ef470.firebaseapp.com",
+	databaseURL: "https://web-app-ef470.firebaseio.com",
+	projectId: "web-app-ef470",
+	storageBucket: "web-app-ef470.appspot.com",
+	messagingSenderId: "42779219816"
+};  
+firebase.initializeApp(config);  
 
-
-var provider = new firebase.auth.GoogleAuthProvider();
-function signIn() {
-	console.log('test');
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  console.log('waiting for response!');
-	  console.log(user);
-	}).catch(function(error) {
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});
-}
+var uiConfig = {  
+	signInSuccessUrl: 'services.html',  
+	signInOptions: [  
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,  
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,  
+		firebase.auth.EmailAuthProvider.PROVIDER_ID  
+	],  
+tosUrl: '<your-tos-url>'  
+};  
+var ui = new firebaseui.auth.AuthUI(firebase.auth());  
+ui.start('#firebaseui-auth-container', uiConfig);  
